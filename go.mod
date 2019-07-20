2410c457ecac19777f5d16aeedfc4958ae900797
Kakha Urigashvili <kurigashvili@conversantmedia.com>
2019-07-20 12:19:47 -0400

    initial swagger set up

--- go.mod
+++ go.mod
@@ -3,11 +3,11 @@ module github.com/kakhaUrigashvili/go-rest-api
 go 1.12
 
 require (
-	github.com/asaskevich/govalidator v0.0.0-20190424111038-f61b66f89f4a
-	github.com/go-ozzo/ozzo-validation v3.5.0+incompatible
+	github.com/alecthomas/template v0.0.0-20160405071501-a0175ee3bccc
 	github.com/gorilla/mux v1.7.3
 	github.com/gorilla/schema v1.1.0
 	github.com/prometheus/client_golang v1.0.0
-	github.com/stretchr/testify v1.3.0
-	github.com/thedevsaddam/govalidator v1.9.8
+	github.com/swaggo/files v0.0.0-20190704085106-630677cd5c14 // indirect
+	github.com/swaggo/http-swagger v0.0.0-20190614090009-c2865af9083e
+	github.com/swaggo/swag v1.6.2
 )
--- main.go
+++ main.go
@@ -7,7 +7,24 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/kakhaUrigashvili/go-rest-api/handler"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/swaggo/http-swagger"
+	_"github.com/kakhaUrigashvili/go-rest-api/docs"
 )
+
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server celler server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8080
+// @BasePath /api/v1
 
 // Main function
 func main() {
@@ -21,6 +38,7 @@ func main() {
 	r.HandleFunc(routePrefix+"/rates", handler.GetRatesHandler).Methods("GET")
 	r.HandleFunc(routePrefix+"/rates", handler.CreateRatesHandler).Methods("POST")
 	r.Handle("/metrics", promhttp.Handler()).Methods("GET")
+	r.PathPrefix("/docs").Handler(httpSwagger.WrapHandler)
 
 	// Start server
 	log.Fatal(http.ListenAndServe(":8000", r))
