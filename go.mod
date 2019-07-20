a97557cc6a25782e622ce1769a92e7a096e2141c
Kakha Urigashvili <kurigashvili@conversantmedia.com>
2019-07-20 11:26:13 -0400

    added prometheus metrics

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/go-ozzo/ozzo-validation v3.5.0+incompatible
 	github.com/gorilla/mux v1.7.3
 	github.com/gorilla/schema v1.1.0
+	github.com/prometheus/client_golang v1.0.0
 	github.com/stretchr/testify v1.3.0
 	github.com/thedevsaddam/govalidator v1.9.8
 )
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/kakhaUrigashvili/go-rest-api/handler"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 // Main function
@@ -19,6 +20,7 @@ func main() {
 	r.HandleFunc(routePrefix+"/rates/search", handler.SearchRateHandler).Methods("GET")
 	r.HandleFunc(routePrefix+"/rates", handler.GetRatesHandler).Methods("GET")
 	r.HandleFunc(routePrefix+"/rates", handler.CreateRatesHandler).Methods("POST")
+	r.Handle("/metrics", promhttp.Handler()).Methods("GET")
 
 	// Start server
 	log.Fatal(http.ListenAndServe(":8000", r))
